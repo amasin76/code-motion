3f4959e5641934da8d6162a962c919509062fb5e
amasin76 <amasin76@gmail.com>
2024-03-06 16:22:16 +0100

    Chore: Add linting & formatting config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,55 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { node: true, browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'simple-import-sort'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
   },
-}
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              // Packages `react` related packages come first, Internal packages.
+              ['^react', '^@?\\w', '^(@|components)(/.*|$)'],
+              // Side effect imports.
+              ['^\\u0000'],
+              // Relative imports (including parent and same-folder)
+              ['^\\.\\.(?!/?$|^\\./?$)', '^\\./(?=.*/)(?!/?$)'],
+              // Style imports.
+              ['^.+\\.?(css)$'],
+            ],
+          },
+        ],
+      },
+    },
+  ],
+  settings: {
+    react: { version: 'detect' },
+  },
+};
