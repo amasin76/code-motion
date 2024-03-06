2ae657242646ea4265c578160a551ab76d6b5c6a
amasin76 <amasin76@gmail.com>
2024-03-06 02:12:53 +0100

    Set-up: Initialize `Shadcn` config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 function App() {
   return (
     <>
       <h1 className="text-2xl font-bold text-red-300">Hello Code-Motion🚀</h1>
+      <Button>Shadcn on</Button>
     </>
   );
 }
