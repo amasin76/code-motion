829fc2090920516c353f150e5ef8e5ee4232b7e8
amasin76 <amasin76@gmail.com>
2024-03-27 12:54:20 +0000

    integrate: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import { ThemeProvider } from '@/provider/theme-provider';
 
 import './index.css';
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Routes />
+      <Analytics />
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
