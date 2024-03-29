1321c0020bf52f38939651a44eec4bfbe4cf4e2f
amasin76 <amasin76@gmail.com>
2024-03-29 07:45:34 +0000

    fix: white screen flash at page loading in dark mode
    - Set default mode to dark
    - Add black background to body

--- src/provider/theme-provider.tsx
+++ src/provider/theme-provider.tsx
@@ -22,12 +22,12 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
 
 export function ThemeProvider({
   children,
-  defaultTheme = 'system',
+  defaultTheme = 'dark',
   storageKey = 'vite-ui-theme',
   ...props
 }: ThemeProviderProps) {
   const [theme, setTheme] = useState<Theme>(
-    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
+    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme,
   );
 
   useEffect(() => {
