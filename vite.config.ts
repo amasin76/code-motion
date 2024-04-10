4e29470e2dd42771bb21ff9b0d1f1ec87da2c163
amasin76 <amasin76@gmail.com>
2024-04-10 12:18:06 +0000

    chore: add `visualizer` plugin for bundle analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig, type PluginOption } from 'vite';
 import prismjs from 'vite-plugin-prismjs';
 
 export default defineConfig({
   plugins: [
     react(),
-    prismjs({
-      languages: 'all',
-    }),
+    prismjs({ languages: 'all' }),
+    visualizer() as PluginOption,
   ],
   resolve: {
     alias: {
